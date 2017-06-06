8762e04e0adf9cee54ebb960331ec98b02fb7fcb
mash <mashimo@val.co.jp>
2017-06-06 15:51:17 +0900

    得点の計算

--- hand.rb
+++ hand.rb
@@ -1,11 +1,21 @@
 class Hand
   def initialize()
-    hand_card
+    @my_cards = []
   end
 
-  def draw()
-    p RANKS.sample
-    p SUITS.sample
+  def draw(deck_card)
+    distributed_card = deck_card.distribute
+    @my_cards << distributed_card
+  end
+
+  def my_cards
+    @my_cards
+  end
+
+  def sum_points
+    splited = @my_cards.map{|w| w.split(":")}
+    num_suplit = splited.map{|w| w[0].to_i}
+    return num_suplit.sum
   end
 
 end
