c1f20e197dcf50a60b905ba8183f35213ac3ab0c
miyabayashi kento <admin@Soumu3noiMac.local>
2017-06-06 16:15:11 +0900

    J,Q,Kを10に変換するテストを追加

--- miyabayashi_and_soeda/spec/lib/black_jack_spec.rb
+++ miyabayashi_and_soeda/spec/lib/black_jack_spec.rb
@@ -13,6 +13,13 @@ describe "Playerクラスは" do
       expect(@player1.calculate).to eq(5)
     end
   end
+  context "J(11)からK(13)が入ってるとき" do
+    it "10として計算する" do
+      @player1.get_card(11)
+      @player1.get_card(12)
+      expect(@player1.calculate).to eq(20)
+    end
+  end
   context "1が1枚入ってるときは" do
     it "1を11として計算する" do
       @player1.get_card(1)
