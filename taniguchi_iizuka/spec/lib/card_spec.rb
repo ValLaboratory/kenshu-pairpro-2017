7a8e80bce9b564830d61932652205ab8bf95086f
taniguchi <k_taniguchi@val.co.jp>
2017-06-06 15:23:40 +0900

    card_spec.rbを修正

--- taniguchi_iizuka/spec/lib/card_spec.rb
+++ taniguchi_iizuka/spec/lib/card_spec.rb
@@ -2,14 +2,10 @@ require_relative "../../lib/card.rb"
 
 describe "Cardクラスの" do
   describe "showメソッドは" do
-    #最初に実行される
-    before do
-      #インスタンス変数として定義している。
-      @card = Card.new("h", 11)
-    end
     context "マークがハートでランクが１１の時" do
       it "h１１の文字列を返す" do
-        expect(@card.show).to eq("h11")
+        card = Card.new("h", 11)
+        expect(card.show).to eq("h11")
       end
     end
   end
